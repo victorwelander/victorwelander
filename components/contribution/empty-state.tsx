24e8115dc7c4f02ec5c7f811c892225dd8a72376
Victor Welander <victortwelander@gmail.com>
2024-09-25 10:42:10 +0200

    fixed icon not being used in empty stat

--- components/contribution/empty-state.tsx
+++ components/contribution/empty-state.tsx
@@ -1,21 +1,23 @@
-type stateType = {
+import React from "react";
+
+type StateType = {
   value?: string;
   icon?: React.ReactNode;
   title?: string;
   message?: string;
 };
 
-export default function EmptyState({ value, title, icon, message }: stateType) {
+export default function EmptyState({ value, title, icon, message }: StateType) {
   return (
     <div className="mt-4 flex w-full flex-col items-center rounded-md border border-dashed border-neutral-300 bg-white px-6 py-8 text-center text-black dark:border-neutral-700 dark:bg-neutral-800 dark:text-white">
-      <div className="mb-6 text-4xl text-zinc-500"></div>
+      {icon && <div className="mb-6 text-4xl text-zinc-500">{icon}</div>}
       <h3 className="mb-3 text-xl font-bold tracking-tight">
         {title ?? `No ${value} Found`}
       </h3>
       <p className="mb-6 ml-4 max-w-sm text-sm text-zinc-500 dark:text-zinc-400">
         {message ??
           `There are no ${
-            value && value.toLowerCase()
+            value ? value.toLowerCase() : "items"
           } available at this time. Check back
         again.`}
       </p>
